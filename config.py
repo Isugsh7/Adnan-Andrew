bf8e4ad6f58ea7dc4935fe8eca855be0bb8a2b67
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2025-05-15 03:26:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pyro import validate_session
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
 
-ss = os.environ.get("TERMUX")
+ss = os.environ.get("1BJWap1sBuxObMZXde7S4nLQ8s1NfrBKMVyDEUVlVVgK3U7y7Y6nmWBVub9IIVufXH1zqAYDE9YW9pYcVr3OTq9tHVoKkLWLD9Ay6NHTRzHuDxcgixCOxtqri5Wfm97o0DODBmHZA3VIaLCQzWKOgq8mt1Bi_8hbc4DJ-FQBO5FiSbNw2hfXuO8506YC0mnBJoK4w7wQcws3_XUa3bEGhwEm52pauS59RBCIBpEpz7VkiCXScyFw73cvauNFF5JLinPhqsvNnW0yFfpxGIMcnnrZshp1K-zXtag_vP_0uiIq-Yxs02QWx12qqqUutE4pu-MdM7AGuzKhIodrWX14vU8ybbrskpik=")
 session = validate_session(ss)
 IEX = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 
