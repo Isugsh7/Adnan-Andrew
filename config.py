81ee81b01cc7ef2b443b7f57b70b5653c98d9258
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2025-05-15 03:27:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from telethon.sessions import StringSession
 import os
 from pyro import validate_session
 
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
+APP_ID = os.environ.get("8400773")
+APP_HASH = os.environ.get("9262ddf84ab2647d2d59c0ba1d60d327")
 
 ss = os.environ.get("1BJWap1sBuxObMZXde7S4nLQ8s1NfrBKMVyDEUVlVVgK3U7y7Y6nmWBVub9IIVufXH1zqAYDE9YW9pYcVr3OTq9tHVoKkLWLD9Ay6NHTRzHuDxcgixCOxtqri5Wfm97o0DODBmHZA3VIaLCQzWKOgq8mt1Bi_8hbc4DJ-FQBO5FiSbNw2hfXuO8506YC0mnBJoK4w7wQcws3_XUa3bEGhwEm52pauS59RBCIBpEpz7VkiCXScyFw73cvauNFF5JLinPhqsvNnW0yFfpxGIMcnnrZshp1K-zXtag_vP_0uiIq-Yxs02QWx12qqqUutE4pu-MdM7AGuzKhIodrWX14vU8ybbrskpik=")
 session = validate_session(ss)
